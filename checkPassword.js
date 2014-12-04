e9924aa732c11176d586c984cc3e89dd2d1d1a80
andreuke <andreatello92@gmail.com>
2014-12-04 02:46:11 -0600

    going on

--- checkPassword.js
+++ checkPassword.js
@@ -1,4 +1,12 @@
-function checkPassword(host, password) {
+function checkPassword(host, password, callback) {
+
+    // TODO: hashare password
+    var hash = hash(password);
+
+
+    chrome.storage.sync.get(password, function(hosts) {
+        console.log(hosts)
+    });
 
     return false;
-}
+};
---
+++ hash.js
@@ -0,0 +1,3 @@
+function hash(password) {
+    return password
+};
