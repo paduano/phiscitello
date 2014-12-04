1a3328f6b70578753df7d32bda1f922ee6c10c7c
andreuke <andreatello92@gmail.com>
2014-12-04 03:17:45 -0600

    going on

--- checkPassword.js
+++ checkPassword.js
@@ -4,17 +4,19 @@ function checkPassword(host, password, callback) {
     var hash = myHash(password);
 
 
-    chrome.storage.sync.get(hash, function(hosts) {
-        if(_.isEmpty(hosts)) {
-            var newHosts = [host];
+    chrome.storage.sync.get(hash, function(returnedValue) {
+        if(_.isEmpty(returnedValue)) {
 
             var obj = {};
-            obj[hash] = newHosts;
+            obj[hash] = [host];
 
-            chrome.storage.sync.set(obj);
-            callback(true);
+            chrome.storage.sync.set(obj, function() {
+                callback(true);
+            });
         }
         else {
+            var hosts = returnedValue[hash];
+
             if(hosts.indexOf(host) != -1) {
                 callback(true);
             }
