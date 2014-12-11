cf19a11c4747cc628bfc24a365225dbc2d41018d
paduano <auerbach98>
2014-12-10 19:29:30 -0600

    default

--- backgroundScript.js
+++ backgroundScript.js
@@ -1,5 +1,18 @@
 $( document ).ready(function() {
 
+    chrome.storage.sync.get('policy', function(returnedValue) {
+        if (_.isEmpty(returnedValue)) {
+
+            var obj = {};
+            obj['policy'] = ['strict'];
+
+            chrome.storage.sync.set(obj, function () {
+                console.log("default policy set to strict");
+                callback(true);
+            });
+        }
+    });
+
     console.log("phiscitello loaded. Now you are safe and sound!");
 
     interceptAllForms();
